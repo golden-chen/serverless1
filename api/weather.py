fee781ea395251bdec665b64c08deb0d2a553ab9
golden-chen <33325840+golden-chen@users.noreply.github.com>
2026-01-08 02:35:52 +0800

    Create weather.py

---
+++ api/weather.py
@@ -0,0 +1,8 @@
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+        self.wfile.write("你好！這是氣象預報訊息。".encode('utf-8'))
